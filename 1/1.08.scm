4cfbe822eddabcefa60d70dac936e21a363c4e27
Josh Clark <joshuamclark@gmail.com>
2020-04-05 11:29:22 -0700

    Exercise 1.8

---
+++ 1/1.08.scm
@@ -0,0 +1,9 @@
+; Use the book procedures except:
+; - Use the good-enough? and sqrt procedures from Exercise 1.7
+; - Replace the improve procedure
+
+(define (improve guess x)
+  (/ (+ (/ x
+	   (square guess))
+	(* 2 guess))
+     3))
