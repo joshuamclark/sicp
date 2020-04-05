5b7d0deca3391443a8bb8c657307f5eb0359a764
Josh Clark <joshuamclark@gmail.com>
2020-04-05 14:52:53 -0700

    Exercises 1.9-10

---
+++ 1/1.10.scm
@@ -0,0 +1,73 @@
+(A 1 10)
+(A 0 (A 1 9))
+(A 0 (A 0 (A 1 8)))
+(A 0 (A 0 (A 0 (A 1 7))))
+(A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+(A 0 (A 0 (A 0 (A 0 64))))
+(A 0 (A 0 (A 0 128)))
+(A 0 (A 0 256))
+(A 0 512)
+1024
+; 2^y when x=1
+
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+(A 1 (A 1 (A 1 2)))
+(A 1 (A 1 (A 0 (A 1 1))))
+(A 1 (A 1 (A 0 2)))
+(A 1 (A 1 4))
+(A 1 (A 0 (A 1 3)))
+(A 1 (A 0 (A 0 (A 1 2))))
+(A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+(A 1 (A 0 (A 0 (A 0 2))))
+(A 1 (A 0 (A 0 4)))
+(A 1 (A 0 8))
+(A 1 16)
+65536
+; 2^16. Looks like 2^(y^x)?
+
+(A 3 3)
+(A 2 (A 3 2))
+(A 2 (A 2 (A 3 1)))
+(A 2 (A 2 2))
+(A 2 (A 1 (A 2 1)))
+(A 2 (A 1 2))
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+(A 1 (A 1 (A 1 2)))
+(A 1 (A 1 4))
+(A 1 16)
+65536
+; Well it's not 2^(y^x). Not sure what this is.
+; 
+; Maybe ((2^y)^y)^y, done x times.
+; - 2^10 = 1024
+; - (2^4)^4 = 2^16 = 65536
+; - No: ((2^3)^3)^3 = 2^27
+; 
+; Ok, this is Knuth's up-arrow notation: 2 ↑^x y.
+; - 2 ↑ 10 = 2^10 = 1024
+; - 2 ↑↑ 4 = 2 ↑ 2 ↑ 2 ↑ 2 = 2^2^2^2 = 2^2^4 = 2^16 = 65536
+; - 2 ↑↑↑ 3 = 2 ↑↑ 2 ↑↑ 2 = 2 ↑↑ (2 ↑ 2) = 2 ↑↑ 4 = 65536
+; 
+; See: https://en.wikipedia.org/wiki/Knuth%27s_up-arrow_notation 
+
+; Procedure definitions:
+; - f(n) = 2n (iterated addition, i.e. multiplication)
+; - g(n) = 2^n (iterated multiplication, i.e. exponentiation)
+; - h(n) = 2 ↑↑ n (iterated exponentiation, i.e. tetration)
+
